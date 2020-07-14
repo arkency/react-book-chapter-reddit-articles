60234e597f6acf40dd713c85a7d3e54e520ed1d7
Tomasz Patrzek <patrzek.tomasz@gmail.com>
2020-07-14 10:49:02 +0200

    migrate to functional components

--- src/components/RedditList.js
+++ src/components/RedditList.js
@@ -1,34 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
-class ArticleList extends React.Component {
-  static defaultProps = {
-    initialArticles: [
-      {
-        title: "What React component class syntax should I use?",
-        url:
-          "http://reactkungfu.com/2015/07/what-react-component-class-syntax-should-i-use/",
-        score: 2
-      },
-      {
-        title: "Why are we using React.js in our projects?",
-        url:
-          "http://reactkungfu.com/2015/07/why-are-we-using-react-js-in-our-projects/",
-        score: 4
-      },
-      {
-        title: "Approaches to testing React components - an overview",
-        url:
-          "http://reactkungfu.com/2015/07/approaches-to-testing-react-components-an-overview/",
-        score: 3
-      }
-    ]
-  };
+function ArticleList(props) {
+  const [articles, updateArticles] = useState(props.initialArticles);
 
-  state = { articles: this.props.initialArticles };
-
-  sortedArticles = () => {
-    let { articles } = this.state;
-
+  const sortedArticles = () => {
     return [].concat(articles).sort((articleOne, articleTwo) => {
       if (articleOne.score == articleTwo.score) return 0;
       if (articleOne.score > articleTwo.score) return -1;
@@ -36,35 +11,57 @@ class ArticleList extends React.Component {
     });
   };
 
-  upvote = articleIndex => {
-    let { articles } = this.state,
-      article = articles[articleIndex];
-
-    article.score = article.score + 1;
-    this.setState({ articles });
+  const upvote = articleIndex => {
+    const article = articles[articleIndex];
+    updateArticles([
+      ...articles.slice(0, articleIndex),
+      { ...article, score: article.score + 1 },
+      ...articles.slice(articleIndex + 1)
+    ]);
   };
 
-  handleUpvoting = article => {
+  const handleUpvoting = article => {
     return ev => {
       ev.preventDefault();
-      this.upvote(this.state.articles.indexOf(article));
+      upvote(articles.indexOf(article));
     };
   };
 
-  render() {
-    return (
-      <ul>
-        {this.sortedArticles().map(article =>
-          <li key={article.url}>
-            ({article.score}) <a href={article.url}>{article.title}</a>{" "}
-            <a href="#" onClick={this.handleUpvoting(article)}>
-              upvote
-            </a>
-          </li>
-        )}
-      </ul>
-    );
-  }
+  return (
+    <ul>
+      {sortedArticles().map(article => (
+        <li key={article.url}>
+          ({article.score}) <a href={article.url}>{article.title}</a>{" "}
+          <a href="#" onClick={handleUpvoting(article)}>
+            upvote
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
 }
+
+ArticleList.defaultProps = {
+  initialArticles: [
+    {
+      title: "What React component class syntax should I use?",
+      url:
+        "http://reactkungfu.com/2015/07/what-react-component-class-syntax-should-i-use/",
+      score: 2
+    },
+    {
+      title: "Why are we using React.js in our projects?",
+      url:
+        "http://reactkungfu.com/2015/07/why-are-we-using-react-js-in-our-projects/",
+      score: 4
+    },
+    {
+      title: "Approaches to testing React components - an overview",
+      url:
+        "http://reactkungfu.com/2015/07/approaches-to-testing-react-components-an-overview/",
+      score: 3
+    }
+  ]
+};
 
 export default ArticleList;
